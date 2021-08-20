57eea44396ea0c97f256eb243e69c9ca76c808f8
Rutvik Kapade <rutvik609@gmail.com>
2021-08-20 11:24:03 +0530

    new commit

---
+++ components/carousel.js
@@ -0,0 +1,5 @@
+export default function Carousel(props){
+    return(
+        <img className="d-block w-100"  src="https://codeforces.com/predownloaded/9d/34/9d345d4d7b74d9d12a23de5813a4a8daa9e110d5.png" alt="First slide"/>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Carousel from '../components/carousel'
 export default function Home() {
 
   return (
     <div>
+      <Carousel/>
      <Link href="/addProducts">
      <a>Add Products</a>
      </Link>
