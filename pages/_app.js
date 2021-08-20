0b549c14ca51a0b5b95db2f43148ee144d1c4c4d
Rutvik Kapade <rutvik609@gmail.com>
2021-08-20 10:56:26 +0530

    new commit

--- pages/_app.js
+++ pages/_app.js
@@ -46,6 +46,7 @@ body : JSON.stringify({
   <Script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></Script>
 <Script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></Script>
 <Script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></Script>
+<Script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></Script>
   <Navbar categories={categories} setCategory={(result)=>{setCurrCategory(result);} } renderMenuOnMount={true}/>
   <Spacer/>
   <div className="product-cart" style={ cart.length==0?{backgroundColor : "#EDEDED"
